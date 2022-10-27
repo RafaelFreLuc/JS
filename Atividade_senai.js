96650c4e3b33cd95e451bac0c1c81cebd3e8a924
RafaelFreLuc <rafaelfreitas.contato@gmail.com>
2022-10-26 21:30:07 -0300

    Atividade_JS

---
+++ Atividade_senai.js
@@ -0,0 +1,25 @@
+
+//
+let peso = 101
+if(peso < 100){
+    console.log("A peça deve ter no minimo 100g")
+}else{ 
+    console.log("A peça possui o peso adequado")
+}
+//
+var listaDePecas = ["Amortecedor", "Motor", "Corpo-Borboleta", "Cambio", "Painel", "Cabeçote"]
+if(listaDePecas.length < 6){
+    console.log("É possivel cadastrar mais peças!")
+}
+else{
+    console.log("Não é possivel cadastrar mais peças")
+}
+//
+let nomePeca = "Dis"
+
+if(nomePeca.length > 3){
+    console.log("A peça possui a quantidade adequada de caracteres")
+}else{
+    console.log("A peça não possui a quantidade adequada de caracteres")
+}
+//
